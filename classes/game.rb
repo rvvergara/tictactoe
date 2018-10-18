6ceb8303ad63a87ba3936c268eb46b2b65dc900e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-18 16:57:12 +0800

    Fix comment positioning and testing

--- classes/game.rb
+++ classes/game.rb
@@ -76,24 +76,27 @@ class Game
     square_select = gets.chomp!
     # the response of the player will first be validated if it's between 0 - 8
     # the response of the player will be checked if it's already unavailable
-
     until square_select.between?("0", "8") || game_board.board.include?(square_select.to_i)
       puts "Hey! invalid input. Select a valid square!"
       square_select = gets.chomp!
     end
-    
-    # if the square is still available then @@board[choice] = player.sign
+    # if the square is still available then board[choice] = player.sign
+    game_board.board[square_select.to_i] = player.sign
     # and then push the choice (convert it first to an integer) to player.moves
+    player.moves.push(square_select.to_i)
     # then call generate_board to visualize new state of the board
-    
+    generate_board(game_board.board)
   end
 
   def game_cycle(player1, player2)
+    turn(player1)
+    turn(player1)
     turn(player1)
     turn(player2)
   end
 
   def check_win(player)
+    
   end
 
   # TODO: remove this demo class
