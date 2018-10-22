e76ee96414598e730d2ce45121697c83e2eb0a90
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-22 11:14:49 +0800

    Test for player and check_win
    - Write test to check if game is able to create Player instance
    - Write test to check if given moves of player, has the player won?

--- test/spec/main_spec.rb
+++ test/spec/main_spec.rb
@@ -1,12 +1,32 @@
 require_relative "../../classes/game"
-require_relative "../../modules/game_actions"
-include GameActions
+
 RSpec.describe Game do
   game = Game.new
-  describe "#game_start" do
+  player = Human.new("Human","X",game)
+  describe "#initialize" do
     it "creates a board" do
-      game.start
-      expect(game.board).to eql([0,1,2,3,4,5,6,7,8])
+      expect(game.board).to eql([0, 1, 2, 3, 4, 5, 6, 7, 8])
     end
   end
+
+  describe "#game_start" do
+    it "is able to create an instance of player" do
+      expect([player.name, player.sign, player.moves]).to eql(["Human", "X", []])
+    end
+  end
+
+  describe "#check_win" do
+    it "can determine whether a player has won or not" do
+      player.moves = [1,2,3]
+      expect(game.check_win(player)).to eql(false)
+      player.moves = [0,1,2]
+      expect(game.check_win(player)).to eql(true)
+    end
+  end
+
 end
+
+# What to test:
+# 1. Instance is creating a board array equal to [0,1,2,3,4,5,6,7,8]
+# 2. Instance of game is able to create Player instance
+# 3. Be able to check who wins based on winning pattern
