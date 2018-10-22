c01fd00406da966408c7ffb52471bc235f7cfd67
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-22 10:33:44 +0600

    fix some stylistic form of ruby on rspec

--- test/spec/main_spec.rb
+++ test/spec/main_spec.rb
@@ -2,8 +2,7 @@ require_relative "../../classes/game"
 
 RSpec.describe Game do
   game = Game.new
-  player = Human.new("Human","X",game)
-  computer = Computer.new("Computer", "O", game)
+  player = Human.new("Human", "X", game)
   describe "#initialize" do
     it "creates a board" do
       expect(game.board).to eql([0, 1, 2, 3, 4, 5, 6, 7, 8])
@@ -26,18 +25,17 @@ RSpec.describe Game do
 
     it "determines if someone wins or it's a draw" do
       robots_game = Game.new
-      robocop = Computer.new("Robocop","X",robots_game)
+      robocop = Computer.new("Robocop", "X", robots_game)
       terminator = Computer.new("Terminator", "O", robots_game)
-      expect(robots_game.game_cycle(robocop,terminator)).to be_between(0,2)
+      expect(robots_game.game_cycle(robocop, terminator)).to be_between(0, 2)
     end
-
   end
 
   # Test check_draw 
   describe "#check_draw" do
     it "can determine whether a game draw" do
       game2 = Game.new
-      game2.board = ['o','x','x','x','x','o','x','o','o']
+      game2.board = %w[o x x x x o x o o]
       expect(game2.check_draw).to eql(true)
     end
   end
@@ -45,12 +43,8 @@ RSpec.describe Game do
   # Test computer input
   describe "#make_choice" do
     it "returns the computer's choice which is a string form of a numeric value between 0 and 8" do
+      computer = Computer.new("Computer", "O", game)
       expect(computer.make_choice).to be_between("0", "8")
     end
   end
 end
-
-# What to test:
-# 1. Instance is creating a board array equal to [0,1,2,3,4,5,6,7,8]
-# 2. Instance of game is able to create Player instance
-# 3. Be able to check who wins based on winning pattern
