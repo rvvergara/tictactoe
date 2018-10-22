39981ccb416c6a041156b22d9879b7b9e3d725ea
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-22 08:44:14 +0600

    Fix game test rspec
      - Remove main.rb file
      - comment game.start

--- test/spec/main_spec.rb
+++ test/spec/main_spec.rb
@@ -1,11 +1,10 @@
-require_relative '../../main'
 require_relative "../../classes/game.rb"
 
 RSpec.describe Game do
   game = Game.new
   describe "#game_start" do
     it "creates a board" do
-      game.start
+      # game.start
       expect(game.board).to eql([0,1,2,3,4,5,6,7,8])
     end
   end
