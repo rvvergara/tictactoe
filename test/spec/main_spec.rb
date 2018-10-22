afc8d325493263d6bfdb6657ad5afaa4ee7ad8a4
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-22 08:49:26 +0600

    Add module in test

--- test/spec/main_spec.rb
+++ test/spec/main_spec.rb
@@ -1,10 +1,11 @@
-require_relative "../../classes/game.rb"
-
+require_relative "../../classes/game"
+require_relative "../../modules/game_actions"
+include GameActions
 RSpec.describe Game do
   game = Game.new
   describe "#game_start" do
     it "creates a board" do
-      # game.start
+      game.start
       expect(game.board).to eql([0,1,2,3,4,5,6,7,8])
     end
   end
