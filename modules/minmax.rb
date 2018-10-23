dbbc8b2d340834aa99888b089bbb64429b62fa69
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-23 09:54:10 +0800

    Set up code to adapat minimax algo for Computer
    - Include as attr_accessor player_one and player_two into the Game class
    - In game_actions.rb make instances of Human and Computer to be instance
    variables
    - Complete minimax.rb
      > it checks terminal states first and returns appropriately depending
    on whether caller wins, loses or it's a draw
     > if no terminal state is found then method will recursively
    alternately call itself on human and computer player
     > when in each recursive call a terminal state is found then the score
    of that terminal state will be passed through above the ancestor call
    > Finally all moves will be stored in a choices array and will be
    minimized or maximized as far as the scores are concerned depending on
    whether the recursive (or main) call is from Human or Computer
    > Final return will be the best choice
    - Include minimax in player.rb and complete Computer subclass

--- modules/minmax.rb
+++ modules/minmax.rb
@@ -1,83 +1,39 @@
-orig_board = ["O", 1, "X", "X", 4, "X", 6, "O", "O"]
-
-$computer = {
-  :sign => "X",
-  :moves => [2, 3, 5],
-}
-$human = {
-  :sign => "O",
-  :moves => [0, 7, 8],
-}
-
-WINNING_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-].freeze
-
-def emptySquares(arr)
-  arr.select { |x| x.is_a? Integer}
-end
-
-def checkWin(player)
-  WINNING_COMBOS.each do |combo|
-    return true if combo.all? {|square| player[:moves].include? square}
-  end
-  false
-end
-
-def minmax(board, player)
-  # Make a copy of orig_board
-  board_state = board.slice(0..-1)
-  # Determine available squares
-  available_squares = emptySquares(board_state)
-  # Store final choice in a hash 
-  choice = {}
-  # Store different choices into an array
-  choices = []
-  # Store opponent in a variable
-  opponent = player == $computer ? $human : $computer
-
-  return {:score => 10} if checkWin($computer) 
-  return {:score => -10} if checkWin($human) 
-  return {:score => 0} if available_squares.size == 0 
-
-  for i in 0...available_squares.size
-    # Store move
-    move = {:index => board_state[available_squares[i]]}
-    # Store in a temporary variable the square which will be played, for resetting later
-    reset_el = board_state[available_squares[i]]
-    # Place move
-    board_state[available_squares[i]] = player[:sign]
-    # Run method recursively and store result of recursive call into result, set move's score property to result's score
-    result = minmax(board_state, opponent)
-    move[:score] = result[:score]
-    choices.push(move)
-    # Reset board_state
-    board_state[available_squares[i]] = reset_el
+module Minimax
+  def empty_squares(arr)
+    arr.select { |x| x.is_a? Integer}
   end
 
-  #Determine computer's best move based on maximizing score in the choices array
-  if(player == $computer) then
-    best = -1000
-    choices.each do |choice|
-      best = choice[:score] if choice[:score] > best
-      return choice  
-    end
-  end 
-
-  if(player == $human) then
-    best = 1000
-    choices.each do |choice|
-      best = choice[:score] if choice[:score] < best
-      return choice  
-    end
-  end 
-end
-
-puts minmax(orig_board, $computer)
+  def minimax(board, player)
+    available_spots = empty_squares(board)
+    # Check for terminal states
+    return {:score => -10} if @parent.check_win(@parent.player_one)
+    return {:score => 10} if @parent.check_win(@parent.player_two)
+    return {:score => 0} if available_spots.size == 0
+    # Collect scores from each empty square
+    choices = []
+    for i in 0...available_spots.size
+      move = {}
+      move[:index] = board[available_spots[i]]
+      player.moves.push(board[available_spots[i]])
+      board[available_spots[i]] = player.sign
+      if player == @parent.player_two then
+        result = minimax(board, @parent.player_one)
+        move[:score] = result[:score]
+      end #end of if
+      if player == @parent.player_one then
+        result = minimax(board, @parent.player_two)
+        move[:score] = result[:score]
+      end #end of if
+      board[available_spots[i]] = move[:index]
+      player.moves.pop
+      choices.push(move)
+    end #end of for loop
+    if player.class == Computer then
+      best = choices.sort {|a, b| b[:score] - a[:score]}[0]
+    end #end of if player.class == Computer
+    if player.class == Human then
+      best = choices.sort {|a, b| a[:score] - b[:score]}[0]
+    end #end of if player.class == Human
+    best
+  end #end of minimax
+end #end of module
